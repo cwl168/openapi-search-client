136f9c08a199f9aaba1c7ae6a987da0784b1b13e
qinjb <279250819@qq.com>
2017-08-01 08:36:52 +0000

    重命名

---
+++ src/Product.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of the boqii openapi search client package.
+ * (c) qinjb <qinjb@boqii.com> liugj <liugj@boqii.com>
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Bqrd\OpenApi\Search;
+
+class Product extends Api
+{
+    /**
+     * __construct 
+     * 
+     * @param string $baseUri 
+     * @param array $options 
+     * 
+     * @access public
+     * 
+     * @return mixed
+     */
+    public function __construct(string $baseUri, array $options)
+    {
+        parent::__construct($baseUri, $options);    
+    }
+
+
+    /**
+     * get 
+     * 
+     * @param array $params 
+     * @param array $headers 
+     * 
+     * @access public
+     * 
+     * @return mixed
+     */
+    public function get(array $param, array $headers = [])
+    {
+         $query = array_intersect_key($param, array_flip([
+                   'q', 'p', 'ps', 's', 'price', 'site_source', 'brandid', 'cateid', 'coupon',
+                   'isstock', 'ifpromotion', 'isglobal', 'attrid', 'source', 'range',
+                  ])
+        );
+        $query['highlight'] = 'pname';
+        $query['facets'] = 'brandid,c3,p';
+        $query['format'] = 'json';
+
+        return ['result' => $this->restClient->get('search', $query, $headers)->toArray()];
+    }
+
+    /**
+     * put 
+     * 
+     * @param mixed $product 
+     * 
+     * @access public
+     * 
+     * @return mixed
+     */
+    public function put($product)
+    {
+        return $this->restClient->put($product)->toArray();
+    }
+}
---
+++ src/Stock.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Bqrd\OpenApi\Search;
+
+class Stock extends Api
+{
+    public function __construct(string $baseUri, array $params)
+    {
+        parent::__construct($baseUri, $params);    
+    }
+
+    public function update($stocks)
+    {
+        
+    }
+}
